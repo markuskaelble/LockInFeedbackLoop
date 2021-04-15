c572b3a5aaee7cfb1df7b8fc6f33f4f8697386a8
markuskaelble <markus.kaelble@outlook.de>
2021-04-15 08:31:45 +0200

    Working Loop

--- main_code/feedback.py
+++ main_code/feedback.py
@@ -3,145 +3,15 @@ import of existing python packages and user-written ones.
 -----------------------------------------------------------------------------------------------------------------------"""
 import numpy as np
 from numpy import array, int64, uint64, uint32, int32, float32, int8, average
-import zhinst 
+import zhinst
+import zhinst.utils
 import pyvisa as visa
 
-class FeedbackLoop( SMIQ, LockIn):
 
-    def __init__(self, signal_input, signal_output, start_frequency):
-        SMIQ()
-        LockIn()
+import time
+import warnings
+import logging
 
-        self.signal_input = signal_input
-        self.signal_output = signal_output
-        self.visa_address = signal_output
-        self.frequency = start_frequency  
-
-        self._run() 
-
-
-
-
-    def _run(self): 
-        """================================variable decleration ================================"""
-        self.outgoingData = 0.0 
-        nan = None     
-
-
-        """-----------------------------------------------------------------------------------------------------------------------
-        Settings for the LockIn, 
-        
-        has been copyed from the zhinst example and slitly adopted. Form the full version checkout:
-        https://docs.zhinst.com/labone_api/python/zhinst.examples.hf2.example_scope.html
-        -----------------------------------------------------------------------------------------------------------------------"""
-        device_id = self.signal_input
-        scope_inputselect=0
-        sigouts_amplitude=0.1
-        module_averaging_weight=1
-        module_historylength=20
-        min_num_records=1
-        apilevel_example = 1
-        err_msg = None 
-
-        """================================API configurations================================"""
-
-        # Call a zhinst utility function that returns:
-        # - an API session `daq` in order to communicate with devices via the data server.
-        # - the device ID string that specifies the device branch in the server's node hierarchy.
-        # - the device's discovery properties.
-        (daq, device, props) = zhinst.utils.create_api_session(device_id, apilevel_example, required_devtype="HF2", required_err_msg=err_msg)
-        zhinst.utils.api_server_version_check(daq)
-        daq.setDebugLevel(3) # Enable the API's log
-        zhinst.utils.disable_everything(daq, device)  # Create a base configuration: Disable all available outputs, awgs, demods, scopes,...
-        """================================Experiment configurations================================"""
-        scope_settings = [[ "/%s/scopes/0/channel" % (device), scope_inputselect],["/%s/scopes/0/bwlimit" % (device), 1],["/%s/scopes/0/time" % (device), 0],["/%s/scopes/0/enable" % device, 1],]
-        daq.set(scope_settings)
-
-        # Perform a global synchronisation between the device and the data server:
-        # Ensure that the settings have taken effect on the device before acquiring data.
-        daq.sync()
-
-        # Now initialize and configure the Scope Module.
-        scopeModule = daq.scopeModule()
-        # 'mode' : Scope data processing mode.
-        # 0 - Pass through scope segments assembled, returned unprocessed, non-interleaved.
-        # 1 - Moving average, scope recording assembled, scaling applied, averaged, if averaging is enabled.
-        # 2 - Not yet supported.
-        # 3 - As for mode 1, except an FFT is applied to every segment of the scope recording.
-        scopeModule.set('scopeModule/mode', 1)
-        # 'averager/weight' : Averager behaviour.
-        #   weight=1 - don't average.
-        #   weight>1 - average the scope record shots using an exponentially weighted moving average.
-        scopeModule.set('scopeModule/averager/weight', module_averaging_weight)
-        # 'historylength' : The number of scope records to keep in the Scope Module's memory, when more records
-        #   arrive in the Module from the device the oldest records are overwritten.
-        scopeModule.set('scopeModule/historylength', module_historylength)
-
-        scope_channel_lookup = {0: 'sigin0', 1: 'sigin1', 2: 'sigout0', 3: 'sigout1'}
-        scope_channel = scope_channel_lookup[scope_inputselect]
-        if scope_channel == 'sigin0':
-            externalscaling = daq.getDouble('/{}/sigins/0/range'.format(device))
-        elif scope_channel == 'sigin1':
-            externalscaling = daq.getDouble('/{}/sigins/1/range'.format(device))
-        elif scope_channel == 'sigout0':
-            externalscaling = daq.getDouble('/{}/sigouts/0/range'.format(device))
-        elif scope_channel == 'sigout1':
-            externalscaling = daq.getDouble('/{}/sigouts/1/range'.format(device))
-        scopeModule.set('scopeModule/externalscaling', externalscaling)
-
-        # Subscribe to the scope's data in the module.
-        wave_nodepath = '/{}/scopes/0/wave'.format(device)
-        scopeModule.subscribe(wave_nodepath)
-
-
-
-        def _readData(self): 
-            """-----------------------------------------------------------------------------------------------------------------------
-            The actual reading function is defined inside LockIn Class, here they are just called. 
-            
-            Notice: 
-            The reading function returns a nested dictionary with several measurements inside. As incomingData, just one of
-            them is returned as Array. 
-            -----------------------------------------------------------------------------------------------------------------------"""
-            result = get_scope_records( self.signal_input, daq, scopeModule, min_num_records)   
-            self.incomingData = array((((result['/%s/scopes/0/wave' % (device)])[0])[0])['wave'][0])
-
-            return self.incomingData
-        
-        
-        
-        def _calculation(self,incomingData): 
-            """-----------------------------------------------------------------------------------------------------------------------
-            Here the actual calculation will be done. 
-            For testing purposes, there is no fancy calculation.   
-            -----------------------------------------------------------------------------------------------------------------------"""
-            self.outgoingData =self.frequency + average(self.incomingData)
-
-            return self.outgoingData
-
-
-
-        def _writeData(self, outgoingData): 
-            """-----------------------------------------------------------------------------------------------------------------------
-            Calls for the SMIQ API to write the new frequency. 
-            -----------------------------------------------------------------------------------------------------------------------"""
-            SMIQ.setFrequency(self, outgoingData)
-            
-            return 
-
-        ############################################################################################################################
-        
-        
-        
-        while True:
-        """-----------------------------------------------------------------------------------------------------------------------
-        The actual infinite loop 
-        -----------------------------------------------------------------------------------------------------------------------"""
-            try:
-                _writeData(self, _calculation(self, _readData(self)))
-                self.frequency =  self.outgoingData 
-                time.sleep(0.001)
-            finally:
 
 
 
@@ -237,13 +107,13 @@ class SMIQ():
 
 
 
-class LockIn():
-        """-----------------------------------------------------------------------------------------------------------------------
-        Subfunctions for the LockIn have been copied from the zhinst example and slightly adopted. Form the full version checkout: 
-        https://docs.zhinst.com/labone_api/python/zhinst.examples.hf2.example_scope.html
-        -----------------------------------------------------------------------------------------------------------------------"""
 
-    def get_scope_records(device, daq, scopeModule, num_records=1):
+"""-----------------------------------------------------------------------------------------------------------------------
+Subfunctions for the LockIn have been copied from the zhinst example and slightly adopted. Form the full version checkout: 
+https://docs.zhinst.com/labone_api/python/zhinst.examples.hf2.example_scope.html
+-----------------------------------------------------------------------------------------------------------------------"""
+
+def get_scope_records(device, daq, scopeModule, num_records=1):
         """
         Obtain scope records from the device using an instance of the Scope Module.
         """
@@ -264,8 +134,8 @@ class LockIn():
             time.sleep(0.5)
             records = scopeModule.getInt("scopeModule/records")
             progress = scopeModule.progress()[0]
-            print(("Scope module has acquired {} records (requested {}). "
-                "Progress of current segment {}%.").format(records, num_records, 100.*progress))
+            #print(("Scope module has acquired {} records (requested {}). "
+            #    "Progress of current segment {}%.").format(records, num_records, 100.*progress))
             data = scopeModule.read(True)
             
             
@@ -284,7 +154,7 @@ class LockIn():
 
         return data
 
-    def check_scope_record_flags(scope_records):
+def check_scope_record_flags(scope_records):
         num_records = len(scope_records)
         for index, record in enumerate(scope_records):
             if record[0]["flags"] & 1:
@@ -300,11 +170,154 @@ class LockIn():
 
 
 
+class FeedbackLoop( SMIQ ):
+
+    def __init__(self, signal_input, signal_output, start_frequency):
+        #SMIQ()
+        
+
+        self.signal_input = signal_input
+        self.signal_output = signal_output
+        self.visa_address = signal_output
+        self.frequency = start_frequency  
+
+        self._run() 
+
+
+
+
+    def _run(self): 
+        """================================variable decleration ================================"""
+        self.outgoingData = 0.0 
+        nan = None     
+
+
+        """-----------------------------------------------------------------------------------------------------------------------
+        Settings for the LockIn, 
+        
+        has been copyed from the zhinst example and slitly adopted. Form the full version checkout:
+        https://docs.zhinst.com/labone_api/python/zhinst.examples.hf2.example_scope.html
+        -----------------------------------------------------------------------------------------------------------------------"""
+        device_id = self.signal_input
+        scope_inputselect=0
+        sigouts_amplitude=0.1
+        module_averaging_weight=1
+        module_historylength=20
+        min_num_records=1
+        apilevel_example = 1
+        err_msg = "This example only supports HF2 Instruments."
+
+        """================================API configurations================================"""
+
+        # Call a zhinst utility function that returns:
+        # - an API session `daq` in order to communicate with devices via the data server.
+        # - the device ID string that specifies the device branch in the server's node hierarchy.
+        # - the device's discovery properties.
+        (daq, device, props) = zhinst.utils.create_api_session(device_id, apilevel_example, required_devtype="HF2", required_err_msg=err_msg)
+        zhinst.utils.api_server_version_check(daq)
+        daq.setDebugLevel(3) # Enable the API's log
+        zhinst.utils.disable_everything(daq, device)  # Create a base configuration: Disable all available outputs, awgs, demods, scopes,...
+        """================================Experiment configurations================================"""
+        scope_settings = [[ "/%s/scopes/0/channel" % (device), scope_inputselect],["/%s/scopes/0/bwlimit" % (device), 1],["/%s/scopes/0/time" % (device), 0],["/%s/scopes/0/enable" % device, 1],]
+        daq.set(scope_settings)
+
+        # Perform a global synchronisation between the device and the data server:
+        # Ensure that the settings have taken effect on the device before acquiring data.
+        daq.sync()
+
+        # Now initialize and configure the Scope Module.
+        scopeModule = daq.scopeModule()
+        # 'mode' : Scope data processing mode.
+        # 0 - Pass through scope segments assembled, returned unprocessed, non-interleaved.
+        # 1 - Moving average, scope recording assembled, scaling applied, averaged, if averaging is enabled.
+        # 2 - Not yet supported.
+        # 3 - As for mode 1, except an FFT is applied to every segment of the scope recording.
+        scopeModule.set('scopeModule/mode', 1)
+        # 'averager/weight' : Averager behaviour.
+        #   weight=1 - don't average.
+        #   weight>1 - average the scope record shots using an exponentially weighted moving average.
+        scopeModule.set('scopeModule/averager/weight', module_averaging_weight)
+        # 'historylength' : The number of scope records to keep in the Scope Module's memory, when more records
+        #   arrive in the Module from the device the oldest records are overwritten.
+        scopeModule.set('scopeModule/historylength', module_historylength)
+
+        scope_channel_lookup = {0: 'sigin0', 1: 'sigin1', 2: 'sigout0', 3: 'sigout1'}
+        scope_channel = scope_channel_lookup[scope_inputselect]
+        if scope_channel == 'sigin0':
+            externalscaling = daq.getDouble('/{}/sigins/0/range'.format(device))
+        elif scope_channel == 'sigin1':
+            externalscaling = daq.getDouble('/{}/sigins/1/range'.format(device))
+        elif scope_channel == 'sigout0':
+            externalscaling = daq.getDouble('/{}/sigouts/0/range'.format(device))
+        elif scope_channel == 'sigout1':
+            externalscaling = daq.getDouble('/{}/sigouts/1/range'.format(device))
+        scopeModule.set('scopeModule/externalscaling', externalscaling)
+
+        # Subscribe to the scope's data in the module.
+        wave_nodepath = '/{}/scopes/0/wave'.format(device)
+        scopeModule.subscribe(wave_nodepath)
+
+
+
+        def _readData(self): 
+            """-----------------------------------------------------------------------------------------------------------------------
+            The actual reading function is defined inside LockIn Class, here they are just called. 
+            
+            Notice: 
+            The reading function returns a nested dictionary with several measurements inside. As incomingData, just one of
+            them is returned as Array. 
+            -----------------------------------------------------------------------------------------------------------------------"""
+            result = get_scope_records( self.signal_input, daq, scopeModule)   
+            self.incomingData = array((((result['/%s/scopes/0/wave' % (device)])[0])[0])['wave'][0])
+
+            return self.incomingData
+        
+        
+        
+        def _calculation(self,incomingData): 
+            """-----------------------------------------------------------------------------------------------------------------------
+            Here the actual calculation will be done. 
+            For testing purposes, there is no fancy calculation.   
+            -----------------------------------------------------------------------------------------------------------------------"""
+            self.outgoingData =self.frequency + average(self.incomingData)
+
+            return self.outgoingData
+
+
+
+        def _writeData(self, outgoingData): 
+            """-----------------------------------------------------------------------------------------------------------------------
+            Calls for the SMIQ API to write the new frequency. 
+            -----------------------------------------------------------------------------------------------------------------------"""
+            SMIQ.setFrequency(self, outgoingData)
+            
+            return 
+
+        ############################################################################################################################
+        
+        
+        
+        while True:
+            """-----------------------------------------------------------------------------------------------------------------------
+            The actual infinite loop 
+            -----------------------------------------------------------------------------------------------------------------------"""
+            try:
+                _writeData(self, _calculation(self, _readData(self)))
+                self.frequency =  self.outgoingData 
+                
+                time.sleep(0.001)
+            finally:
+                print self.frequency
+                pass
+
+
+
+
+
 """-----------------------------------------------------------------------------------------------------------------------
 Every time main.py is called this function will execute the class above. The call for the class should be implemented in the main program. 
 Here it just added for testing purposes.
 -----------------------------------------------------------------------------------------------------------------------"""
-"""
+
 if __name__ == "__main__":
     FeedbackLoop("dev1492", "GPIB0::28", 1e6 )
-"""
